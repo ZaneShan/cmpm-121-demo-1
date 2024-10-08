b0fca57c9c5657288404b8ae92fec4ae5e730ebe
ZaneShan <dchen113@ucsc.edu>
2024-10-08 16:09:06 -0700

    step 2

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "gamename";
+const gameName = "MoonCake Clicker";
 document.title = gameName;
 
 const header = document.createElement("h1");
@@ -12,3 +12,14 @@ app.append(header);
 const button = document.createElement("button");
 button.innerHTML = "🥮";
 app.append(button);
+
+const counter = document.createElement("div");
+let count: number = 0;
+counter.innerHTML = `${count} Mooncakes`;
+counter.className = "counter";
+app.append(counter);
+
+button.addEventListener("click", () => {
+    count++;  // Increment the counter
+    counter.innerHTML = `${count} Mooncakes`; // Update the message
+  });
