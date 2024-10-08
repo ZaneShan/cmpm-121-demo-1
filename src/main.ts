b970bd9acb2faaa0be55dd950ec6fcfbd3c60b13
ZaneShan <dchen113@ucsc.edu>
2024-10-08 16:00:06 -0700

    Step 1 / temporarily removed husky to fix commit errors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "ZaneGame";
+const gameName = "gamename";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = "🥮";
+app.append(button);
