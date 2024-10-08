7352ed640b7227af0a79bcad5022a1ccb96498a4
ZaneShan <dchen113@ucsc.edu>
2024-10-08 16:10:14 -0700

    step 3

--- src/main.ts
+++ src/main.ts
@@ -23,3 +23,9 @@ button.addEventListener("click", () => {
     count++;  // Increment the counter
     counter.innerHTML = `${count} Mooncakes`; // Update the message
   });
+
+  // Automatically increment the counter by 1 every second using setInterval
+setInterval(() => {
+    count++;  // Increment the counter every second
+    counter.innerHTML = `${count} Mooncakes`;  // Update the counter display
+  }, 1000);  // 1000 ms = 1 second
